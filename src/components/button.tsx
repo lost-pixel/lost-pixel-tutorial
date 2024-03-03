a527547c3d547f464ac50b817aac09cb70976da6
d-ivashchuk <dimaiv@me.com>
2024-03-03 12:29:23 +0100

    remove style prop from button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,15 +1,13 @@
 type Props = {
   label: string;
   onClick?: () => unknown;
-  style?: React.CSSProperties;
 };
 
-export const Button = ({ label, onClick = () => null, style }: Props) => {
+export const Button = ({ label, onClick = () => null }: Props) => {
   return (
     <button
       onClick={onClick}
       className="transform rounded-md bg-blue-600 px-8 py-2 capitalize tracking-wide text-white transition-colors duration-200 hover:bg-blue-500 focus:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
-      style={style}
     >
       {label}
     </button>
