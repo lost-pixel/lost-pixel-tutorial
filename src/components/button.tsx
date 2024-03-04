adeb5124df53e16b08d9979e6726e0850ee4b818
d-ivashchuk <dimaiv@me.com>
2024-03-04 19:35:33 +0100

    commit button style nuke

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,11 +4,10 @@ type Props = {
   style?: React.CSSProperties;
 };
 
-export const Button = ({ label, onClick = () => null, style }: Props) => {
+export const Button = ({ label, onClick = () => null }: Props) => {
   return (
     <button
       onClick={onClick}
-      style={style}
       className="transform rounded-md bg-blue-600 px-8 py-2 capitalize tracking-wide text-white transition-colors duration-200 hover:bg-blue-500 focus:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
     >
       {label}
