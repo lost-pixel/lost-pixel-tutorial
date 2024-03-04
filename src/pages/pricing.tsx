63e4d7f9dc43f622d9fcbf9126bab11764ac4480
d-ivashchuk <dimaiv@me.com>
2024-03-04 19:45:20 +0100

    change pricing title

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -9,7 +9,7 @@ const Pricing: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center bg-gray-900">
         <div className="container flex flex-col items-center justify-center gap-2 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Pricing
+            Awesome Pricing
           </h1>
           <div className="container">
             <PricingList />
